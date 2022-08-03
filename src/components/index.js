30b46c5c088ec7d1b57aa1b2e4e41711737e7cb7
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-03 16:10:51 +0100

    RepoInfo component added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Search } from './Search';
 export { default as User } from './User';
 export { default as ReposList } from './ReposList';
+export { default as RepoInfo } from './RepoInfo';
--- src/layout/infoDetails/index.js
+++ src/layout/infoDetails/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { ReposList } from '../../components'
+import { RepoInfo, ReposList } from '../../components'
 
 export default function InfoDetails() {
   return (
     <div>infoDetails:
       <ReposList />
+      <RepoInfo />
     </div>
   )
 }
