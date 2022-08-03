82a298fe6564325cbef30c88eab222eb9f1a0204
daiana.dobre@getfutureproo.co.uk <daiana_melania@yahoo.com>
2022-08-03 16:05:48 +0100

    user component added

--- src/layout/userInfo/index.js
+++ src/layout/userInfo/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { User } from '../../components';
 
 
 export default function UserInfo() {
   return (
-    <div>userInformation</div>
+    <div>userInformation:
+      <User />
+    </div>
   )
 }
